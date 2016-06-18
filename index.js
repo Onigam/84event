c421b63fd82d679a6bddc82de2eb55635cde4a57
Magino <magino@castr.com>
2016-06-18 15:38:43 +0200

    first react components

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+require('babel-register')();
+
+var server = require('./server.jsx').default;
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, function () {
+  console.log('Server listening on', PORT);
+});
---
+++ shared/components/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+export default class App extends React.Component {
+  render() {
+    return (
+      <div id="app-view">
+        <h1>84 events</h1>
+        <hr />
+        {this.props.children}
+      </div>
+    );
+  }
+}
