5c9e6242a8a02e8971e9465f206fe6d342c19e64
Magino <magino@castr.com>
2016-06-25 16:01:54 +0200

    changing default port for heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require('babel-register')();
 
 var server = require('./server.jsx').default;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, function () {
   console.log('Server listening on', PORT);
 });
